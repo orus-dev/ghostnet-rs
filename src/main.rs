7aa69c6ea9d9d041f2bd1dcda3b7167f0623fd3e
Kleo Dev <klesti.selimay@icloud.com>
2025-08-12 19:29:12 -0400

    Changed version and main file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-use ghostnet_rs::GhostStream;
-
 fn main() {
-    let mut stream = GhostStream::connect((0, 0, 0, 0), 7525).unwrap();
-    stream.write("Hello, World".as_bytes()).unwrap();
-    let mut data = [0; 12];
-    let data_len = stream.read(&mut data).unwrap();
-    if data_len == 0 {
-        return;
-    }
-
-    println!("From server: {}", String::from_utf8(data.to_vec()).unwrap());
+    ghostnet_rs::gateway::run().unwrap();
 }
